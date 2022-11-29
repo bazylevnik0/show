dd1855b763931a16e4427724c2074ae53be7ba8b
bazylevnik0 <bazylevnik0@gmail.com>
2022-11-29 19:53:57 +0300

    mold ready

--- show.c
+++ show.c
@@ -1,6 +1,9 @@
 /* Creates a negative image of the input bitmap file */
-#include "qdbmp.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "qdbmp.h"
 
 int main( int argc, char* argv[] )
 {
@@ -9,12 +12,11 @@ int main( int argc, char* argv[] )
     UINT    width, height;
     UINT    x, y;
 
-    if ( argc != 3 )
+    if ( argc != 2 )
     {
-        fprintf( stderr, "Usage: %s <input file> <output file>\n", argv[ 0 ] );
+        fprintf( stderr, "Usage: %s <input file>\n", argv[ 0 ] );
         return 0;
     }
-
     /* Read an image file */
     bmp = BMP_ReadFile( argv[ 1 ] );
     BMP_CHECK_ERROR( stderr, -1 ); /* If an error has occurred, notify and exit */
@@ -23,23 +25,176 @@ int main( int argc, char* argv[] )
     width = BMP_GetWidth( bmp );
     height = BMP_GetHeight( bmp );
 
-    /* Iterate through all the image's pixels */
-    for ( x = 0 ; x < width ; ++x )
-    {
-        for ( y = 0 ; y < height ; ++y )
-        {
-            /* Get pixel's RGB values */
-            BMP_GetPixelRGB( bmp, x, y, &r, &g, &b );
+    //We choose random points for infection of spores (white pixels, with trees)
+    //Spores start eat and grow -> black points and near-black areas
+    //Active grow white areas , black points grow to gray area
+    //New black pixels(many) and reepeat, old gray area transform to green, white continue grow
 
-            /* Invert RGB values */
-            BMP_SetPixelRGB( bmp, x, y, 255 - r, 255 - g, 255 - b );
+    //random seed
+    srand(time(NULL)); 
+    r = 0;
+            //time
+    for (int t = 0; t < 100; t++) 
+    {
+        //step 1
+        if (t >= 0 && t < 10) 
+        {
+            for ( x = 0 ; x < width ; ++x )
+            {
+                /* Iterate through all the image's pixels */
+                for ( y = 0 ; y < height ; ++y )
+                {
+                    r = rand() % 2147483647;
+                    if (r == 0) 
+                    {
+                        /* Change RGB values */
+                        BMP_SetPixelRGB( bmp, x, y, 255, 255, 255 );
+                    }
+                }
+            }
         }
+        //step 2
+        if (t >= 10 && t < 30)
+        {
+            for ( x = 0 ; x < width ; ++x )
+            {
+                /* Iterate through all the image's pixels */
+                for ( y = 0 ; y < height ; ++y )
+                {
+                    r = rand() % 2147483647;
+                    if (r == 0) 
+                    {
+                        r = rand() % 2147483647;
+                        if (r == 0) 
+                        {
+                    
+                            /* Change RGB values */
+                            BMP_SetPixelRGB( bmp, x, y, 0, 0, 0 );
+                            BMP_SetPixelRGB( bmp, x+1, y, 0, 0, 0 );
+                            BMP_SetPixelRGB( bmp, x+1, y-1, 0, 0, 0 );
+                            BMP_SetPixelRGB( bmp, x, y+1, 0, 0, 0 );
+                            BMP_SetPixelRGB( bmp, x-1, y-1, 0, 0, 0 );
+                            BMP_SetPixelRGB( bmp, x-1, y, 0, 0, 0 );
+                            BMP_SetPixelRGB( bmp, x-1, y+1, 0, 0, 0 );
+                            BMP_SetPixelRGB( bmp, x, y+1, 0, 0, 0 );
+                            BMP_SetPixelRGB( bmp, x+1, y+1, 0, 0, 0 );
+                        }
+                    }
+                }
+            }
+        }
+        //step3
+        if (t >= 30 && t < 60)
+        {
+            for ( x = 0 ; x < width ; ++x )
+            {
+                /* Iterate through all the image's pixels */
+                for ( y = 0 ; y < height ; ++y )
+                {
+                    r = rand() % 2;
+                    if (r == 0) 
+                    {   
+                        /* Get pixel's RGB values */
+                        BMP_GetPixelRGB( bmp, x, y, &r, &g, &b );
+                        //if get pixel black or gray
+                        if ((r == 0 && g == 0 && b == 0)||(r == 192 && g == 192 && b == 192))
+                        {
+                            //if r == 0 then add black else gray
+                            r = rand() % 2;
+                            if (r == 0) 
+                            {  
+                                    //if r == 0 then grow to + else to -
+                                    r = rand() % 2;
+                                    if (r == 0) 
+                                    {
+                                        BMP_SetPixelRGB( bmp, x+1, y, 0, 0, 0 );
+                                        BMP_SetPixelRGB( bmp, x, y+1, 0, 0, 0 );
+                                    }
+                                    if (r == 1) 
+                                    {
+                                        BMP_SetPixelRGB( bmp, x-1, y, 0, 0, 0 );
+                                        BMP_SetPixelRGB( bmp, x, y-1, 0, 0, 0 );
+                                    }
+                                
+                            }
+                            if (r == 1) 
+                            {  
+                                //if r == 0 then grow to + else to -
+                                r = rand() % 2;
+                                if (r == 0) 
+                                {
+                                    BMP_SetPixelRGB( bmp, x+1, y, 192, 192, 192 );
+                                    BMP_SetPixelRGB( bmp, x, y+1, 192, 192, 192 );
+                                }
+                                if (r == 1) 
+                                {
+                                    BMP_SetPixelRGB( bmp, x-1, y, 192, 192, 192 );
+                                    BMP_SetPixelRGB( bmp, x, y-1, 192, 192, 192 );
+                                }
+                            }                      
+                        }
+                        //and grow white pixels
+                        if (r == 255 && g == 255 && b == 255) 
+                        {
+                            r = rand() % 2;
+                            if (r == 0)
+                            {
+                                //if r == 0 then grow to + else to -
+                                r = rand() % 2;
+                                if (r == 0) 
+                                {
+                                    BMP_SetPixelRGB( bmp, x+1, y, 255, 255, 255 );
+                                    BMP_SetPixelRGB( bmp, x, y+1, 255, 255, 255 );
+                                }
+                                if (r == 1) 
+                                {
+                                    BMP_SetPixelRGB( bmp, x-1, y, 255, 255, 255 );
+                                    BMP_SetPixelRGB( bmp, x, y-1, 255, 255, 255 );
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        //step4
+        if (t >= 60 && t < 100)
+        {
+            for ( x = 0 ; x < width ; ++x )
+            {
+                /* Iterate through all the image's pixels */
+                for ( y = 0 ; y < height ; ++y )
+                {
+                    r = rand() % 100;
+                    if (r == 0) 
+                    {   
+                        //if r == 0 then grow to + else to -
+                        r = rand() % 2;
+                        if (r == 0) 
+                        {
+                            BMP_SetPixelRGB( bmp, x+1, y, 107, 142, 35 );
+                            BMP_SetPixelRGB( bmp, x, y+1, 107, 142, 35 );
+                        }
+                        if (r == 1) 
+                        {
+                            BMP_SetPixelRGB( bmp, x-1, y, 107, 142, 35 );
+                            BMP_SetPixelRGB( bmp, x, y-1, 107, 142, 35 );
+                        }
+                    }
+                }
+            }
+        }           
+        /* Save result */
+        char name[20] = "./output/output";
+        char num[3];
+        sprintf(num, "%d", t);
+        strcat(name,num);
+        strcat(name,".bmp");
+        BMP_WriteFile( bmp, name );
+        BMP_CHECK_ERROR( stderr, -2 );
     }
 
-    /* Save result */
-    BMP_WriteFile( bmp, argv[ 2 ] );
-    BMP_CHECK_ERROR( stderr, -2 );
-
+   
     /* Free all memory allocated for the image */
     BMP_Free( bmp );
 
